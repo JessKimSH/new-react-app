b806dc41dfaf3d8998a38a49516affb1c46fea42
Sanghun Kim <jesskim@hyundai-autoever.com>
2019-04-30 16:22:43 +0900

    Added tutorials

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+Meteor.methods({
+
+})
